66def1b075d44c2f4228a2b542235fd43d1fa81c
SylveonDev <sylveondev@yahoo.com>
2024-01-16 22:46:58 -0500

    Forgot to change SylveonBot branding

--- commands/general/about.js
+++ commands/general/about.js
@@ -22,7 +22,7 @@ class AboutCommand extends Command {
   async messageRun(message) {
     const embed = new EmbedBuilder()
     .setTitle(`About ${this.container.client.user.username}`)
-    .setDescription(`PhoenixBot is an open-source bot by SylveonDev that features moderation, utilities, and some fun. The bot is based on discord.js and the sapphire command framework. Requires nodejs and mongodb atlas to function.\n\nBot configuration\nBot Version: \` ${require('../../package.json').version} \`    Commands: \`${this.container.applicationCommandRegistries.acquire.length}\`\n\n[\`[Github link]\`](https://github.com/SylveonDev/SylveonBot)    [\`[Sapphire]\`](https://www.sapphirejs.dev)    [\`[Discord server]\`](https://discord.gg/efE9c9AkWy)`)
+    .setDescription(`PhoenixBot is an open-source bot by SylveonDev that features moderation, utilities, and some fun. The bot is based on discord.js and the sapphire command framework. Requires nodejs and mongodb atlas to function.\n\nBot configuration\nBot Version: \` ${require('../../package.json').version} \`    Commands: \`${this.container.applicationCommandRegistries.acquire.length}\`\n\n[\`[Github link]\`](https://github.com/SylveonDev/PhoenixBot)    [\`[Sapphire]\`](https://www.sapphirejs.dev)    [\`[Discord server]\`](https://discord.gg/efE9c9AkWy)`)
     .setThumbnail(this.container.client.user.displayAvatarURL({ extension:"png", size:512 }))
     .setFooter({ text: 'Bot booted up', iconURL: 'https://media.discordapp.net/attachments/1007635929629995070/1187822905212543077/timer-icon-131.png' })
     .setTimestamp(this.container.client.readyTimestamp)
