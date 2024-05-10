fa19b373d502ede7272a6541c49757ba41939d44
SylveonDev <sylveondev@yahoo.com>
2024-05-09 21:00:49 -0400

    Forgot to save this

--- listeners/messageCommandDenied.js
+++ listeners/messageCommandDenied.js
@@ -1,6 +1,13 @@
 const { Listener } = require('@sapphire/framework');
 
 class MessageCommandDenied extends Listener {
+    constructor(context, options) {
+        super(context, {
+          ...options,
+          once: false,
+          event: 'messageCommandDenied'
+        });
+      }
     run(error, { message }) {
         return message.reply({ content: `:x: ${error}` });
     }
