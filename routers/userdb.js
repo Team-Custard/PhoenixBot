402be8c9c57c871526620191fdcc39dc81042bd0
SylveonDev <sylveondev@yahoo.com>
2024-05-15 02:11:02 -0400

    Base UserDB website

---
+++ routers/userdb.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const UserDB = require('../tools/UserDB');
+
+const router = express.Router();
+
+router.get('/u/:id', async function(req, res) {
+    const member = req.params.id;
+    const usersettings = await UserDB.findById(member, UserDB.upsert).cacheQuery();
+    if (!usersettings) return res.status(404).render(`errors/userdb`, { title: "Error", userdberr: "This user was not found in the UserDB database. They may not have setup UserDB yet." });
+
+    const user = await require('../bot').client.users.fetch(member);
+    if (!user) return res.status(404).render(`errors/userdb`, { title: "Error", userdberr: "This user no longer exists on Discord." });
+
+    await res.render("pages/userdb/page", { title: "UserDB", uid: member, uset: usersettings, us: user });
+});
+
+module.exports = router;
