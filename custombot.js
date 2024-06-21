884fab37e4cb121ca529e3fa2d957ef5c756adc1
SylveonDev <sylveondev@yahoo.com>
2024-06-21 17:14:09 -0400

    Fix normal phoenix checking for custom

--- custombot.js
+++ custombot.js
@@ -1,18 +1,18 @@
 const { PhoenixClient } = require("./tools/PhoenixClient");
-const fs = require('fs');
+const fs = require("fs");
 let clients = [];
 
 process.on(`message`, function (msg) {
-    const theowner = JSON.parse(
-        fs.readFileSync(__dirname+"/custombots.json", "utf8"),
-        ).find(b => b.token==msg);
-    if (theowner) {
-        exports.owner = theowner;
-        require("./tools/database").connect();
-        const client = new PhoenixClient();
-        client.login(msg);
-    }
-})
+  const theowner = JSON.parse(
+    fs.readFileSync(__dirname + "/custombots.json", "utf8"),
+  ).find((b) => b.token == msg);
+  if (theowner) {
+    exports.owner = theowner;
+    require("./tools/database").connect();
+    const client = new PhoenixClient();
+    client.login(msg);
+  }
+});
 
 exports.clients = clients;
-exports.list = __dirname+"/custombots.json";
+exports.list = __dirname + "/custombots.json";
