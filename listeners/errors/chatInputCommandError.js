4f1934c0cdbc6594b87d7babb2893da0dece1263
SylveonDev <sylveondev@yahoo.com>
2024-08-08 21:00:33 -0400

    Send errors through webhooks

--- listeners/errors/chatInputCommandError.js
+++ listeners/errors/chatInputCommandError.js
@@ -1,4 +1,5 @@
 const { Listener } = require("@sapphire/framework");
+const { EmbedBuilder, Colors, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 class chatInputCommandError extends Listener {
   constructor(context, options) {
@@ -8,7 +9,43 @@ class chatInputCommandError extends Listener {
       event: "chatInputCommandError",
     });
   }
-  run(error, { interaction }) {
+  async run(error, { interaction }) {
+    if (require('../../config.json').process.errorwebhook) {
+      const embed = new EmbedBuilder()
+      .setAuthor({
+        name: this.container.client.user.username,
+        iconURL: this.container.client.user.displayAvatarURL({dynamic:true})
+      })
+      .setTitle(`Error occured while running the slash command ${interaction.commandName}`)
+      .setDescription(
+        `\`\`\`${error.stack}\`\`\``
+      )
+      .setColor(Colors.Orange)
+      .setFooter({ text: `Error occured` })
+      .setTimestamp(new Date())
+
+      const actionRow = new ActionRowBuilder()
+      .addComponents(new ButtonBuilder()
+        .setStyle(ButtonStyle.Link)
+        .setLabel(`Fix the bug`)
+        .setURL(`https://github.com/team-custard/phoenixbot/tree/staging`)
+      )
+
+      const channel = await this.container.client.channels.fetch(process.env["errwebhookid"])
+      .catch(() => undefined);
+      if (channel) {
+        const webhook = await require('../../tools/webhookFetch').find(channel);
+        if (webhook) webhook.send({
+          username: this.container.client.user.username,
+          avatarURL: this.container.client.user.displayAvatarURL({size: 512, extension: "png"}),
+          embeds: [embed],
+          components: [actionRow]
+        }).then(() => console.log(`Webhook sent`))
+        .catch(() => console.log(`Error while sending error webhook`));
+      }
+    }
+    else console.error(error);
+
     if (interaction.deferred || interaction.replied) {
       return interaction.editReply({
         content: `${this.container.emojis.error} ${error}`,
